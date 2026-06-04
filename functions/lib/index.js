78edf03e3e535d7193da69b4f03df105cdd877b9
Vavina-Vittorem <vavina@mikiosko.mx>
2026-06-04 13:16:14 -0600

    chore: remove redundant vite configs and compiled cloud functions, add functions gitignore

--- functions/lib/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const admin = require("firebase-admin");
-admin.initializeApp();
-// Las Cloud Functions se definirán aquí.
-//# sourceMappingURL=index.js.map
